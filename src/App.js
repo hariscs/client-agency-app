12c256a719733e17af89bc21fcba47a74d2f364c
Haris Shah <harisshah385@gmail.com>
2022-02-15 12:06:13 +0500

    add 404 page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
+import NotFound from './pages/NotFound';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Routes>
+				<Route path='*' element={<NotFound />} />
 				<Route path='/services' element={<Services />} />
 				<Route path='/about' element={<About />} />
 				<Route path='/' element={<Home />} />
