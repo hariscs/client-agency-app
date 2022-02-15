87c83e90c851f4fbe1f30f61d5800c7d0b2179a7
Haris Shah <harisshah385@gmail.com>
2022-02-15 10:51:56 +0500

    add team section

--- src/components/about/Team.jsx
+++ src/components/about/Team.jsx
@@ -1,14 +1,27 @@
 import styles from '../../styles/components/about/Team.module.css';
-import { images } from '../../assets/data';
+import img1 from '../../assets/1.png';
+import img2 from '../../assets/2.png';
+import img3 from '../../assets/3.png';
+import img4 from '../../assets/4.png';
 
 const Team = () => {
 	return (
 		<section className={styles.team}>
 			<div className='container'>
-				{/* {images.map((img, index) => (
-					<img src={img} alt='team' key={index} />
-				))} */}
-				team
+				<div className={styles.images__grid}>
+					<div className={styles.img__col}>
+						<img src={img1} alt='team person' className={styles.img__mt} />
+					</div>
+					<div className={styles.img__col}>
+						<img src={img2} alt='team person' className={styles.img__mb} />
+					</div>
+					<div className={styles.img__col}>
+						<img src={img3} alt='team person' className={styles.img__mt} />
+					</div>
+					<div className={styles.img__col}>
+						<img src={img4} alt='team person' className={styles.img__mb} />
+					</div>
+				</div>
 			</div>
 		</section>
 	);
