4e0837666278844bf69be4e07f20d87ab742b6ed
Haris Shah <harisshah385@gmail.com>
2022-02-15 11:57:40 +0500

    add add hover effect on images

--- src/components/about/Team.jsx
+++ src/components/about/Team.jsx
@@ -11,15 +11,31 @@ const Team = () => {
 				<div className={styles.images__grid}>
 					<div className={styles.img__col}>
 						<img src={img1} alt='team person' className={styles.img__mt} />
+						<div className={styles.img__info}>
+							<h3 className={styles.img__title}>Tayyab</h3>
+							<p className={styles.img__subtitle}>CEO</p>
+						</div>
 					</div>
 					<div className={styles.img__col}>
 						<img src={img2} alt='team person' className={styles.img__mb} />
+						<div className={styles.img__info}>
+							<h3 className={styles.img__title}>Hamza</h3>
+							<p className={styles.img__subtitle}>Product Manager</p>
+						</div>
 					</div>
 					<div className={styles.img__col}>
 						<img src={img3} alt='team person' className={styles.img__mt} />
+						<div className={styles.img__info}>
+							<h3 className={styles.img__title}>Waqas</h3>
+							<p className={styles.img__subtitle}>Consultant</p>
+						</div>
 					</div>
 					<div className={styles.img__col}>
 						<img src={img4} alt='team person' className={styles.img__mb} />
+						<div className={styles.img__info}>
+							<h3 className={styles.img__title}>Usama</h3>
+							<p className={styles.img__subtitle}>Content Writter</p>
+						</div>
 					</div>
 				</div>
 			</div>
